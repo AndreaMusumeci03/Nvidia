3d90f9652c81609e540f123ad5b11bf028bc77f1
AndreaMusumeci03 <andreact945@gmail.com>
2025-09-10 18:14:24 +0200

    Bootstrap Laravel Nvidia

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HW1 - Home</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<nav>
+    <a href="{{ route('home') }}">Home</a>
+</nav>
+<body>
+    
+</body>
+</html>
